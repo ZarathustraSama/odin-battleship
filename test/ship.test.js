8111cbe46405ea0fb55542098dff7035390998a4
ZarathustraSama <mr.gabriel@live.it>
2024-11-25 16:02:57 +0100

    All tests pass

--- test/ship.test.js
+++ test/ship.test.js
@@ -5,6 +5,12 @@ test('Ship gets hit', () => {
 
   ship.hit();
   expect(ship.hits).toBe(1);
-  ship.hit()
+  ship.hit();
   expect(ship.hits).toBe(2);
+ })
+
+ test('Correctly detects when is or is not sunk', () => {
+  const ship = new Ship(3);
+
+  expect(ship.isSunk()).toBe(false);
  })
