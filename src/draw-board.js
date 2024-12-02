1a40dee4dfd2841bae4e0754d10f79074ba17968
ZarathustraSama <mr.gabriel@live.it>
2024-12-02 17:45:29 +0100

    First commit, only adds rows/cells

--- src/draw-board.js
+++ src/draw-board.js
@@ -1,3 +1,29 @@
+import Ship from "./ship";
+
 function drawBoard(board) {
-  // DOM manipulation with innitial event listeners
+  const table = document.querySelector("#player-1 table");
+  for (const row of board) {
+    const tr = document.createElement("tr");
+    for (const cell of row) {
+      const td = document.createElement("td");
+      if (cell instanceof Ship) {
+        td.classList = "ship";
+        td.addEventListener("click", () => {
+          drawAttackSuccess
+        })
+      }
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
 }
+
+function drawAttackFail() {
+
+}
+
+function drawAttackSuccess() {
+
+}
+
+export default drawBoard;
