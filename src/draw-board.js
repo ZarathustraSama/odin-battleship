dd7ebe15746d3888f2c915747f11de8de8f72b3f
ZarathustraSama <mr.gabriel@live.it>
2024-12-04 19:43:30 +0100

    Working as intended, draws the battle grid, and adds its event listeners for attacks

--- src/draw-board.js
+++ src/draw-board.js
@@ -1,29 +1,43 @@
 import Ship from "./ship";
 
-function drawBoard(board) {
-  const table = document.querySelector("#player-1 table");
-  for (const row of board) {
-    const tr = document.createElement("tr");
-    for (const cell of row) {
-      const td = document.createElement("td");
-      if (cell instanceof Ship) {
-        td.classList = "ship";
-        td.addEventListener("click", () => {
-          drawAttackSuccess
-        })
+function drawBoard(player) {
+  const board = player.board.board;
+  const grid = player.type == "Real" ? document.querySelector("#player-1 div") : document.querySelector("#player-2 div");
+  grid.innerHTML = "";
+  for (let iR = 0; iR < 10; iR++) {
+    for (let iC = 0; iC < 10; iC++) {
+      const cell = document.createElement("div");
+      if (board[iR][iC] instanceof Ship) {
+        cell.classList = player.type == "Real" ? `cell ship-${board[iR][iC].length}` : "cell";
+        cell.addEventListener("click", () => { drawAttackSuccess(player, cell, [iR, iC]); }, { once: true });
       }
-      tr.appendChild(td);
+      else {
+        cell.classList = "cell";
+        cell.addEventListener("click", () => { drawAttackFail(player, cell, [iR, iC]); }, { once: true } );
+      }
+      grid.appendChild(cell);
     }
-    table.appendChild(tr);
   }
 }
 
-function drawAttackFail() {
+function drawAttackFail(player, cell, coordinates) {
+  player.board.receiveAttack(coordinates);
 
+  cell.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>sword-cross</title><path d='M6.2,2.44L18.1,14.34L20.22,12.22L21.63,13.63L19.16,16.1L22.34,19.28C22.73,19.67 22.73,20.3 22.34,20.69L21.63,21.4C21.24,21.79 20.61,21.79 20.22,21.4L17,18.23L14.56,20.7L13.15,19.29L15.27,17.17L3.37,5.27V2.44H6.2M15.89,10L20.63,5.26V2.44H17.8L13.06,7.18L15.89,10M10.94,15L8.11,12.13L5.9,14.34L3.78,12.22L2.37,13.63L4.84,16.1L1.66,19.29C1.27,19.68 1.27,20.31 1.66,20.7L2.37,21.41C2.76,21.8 3.39,21.8 3.78,21.41L7,18.23L9.44,20.7L10.85,19.29L8.73,17.17L10.94,15Z' /></svg>"
+  cell.classList = "cell missed"
 }
 
-function drawAttackSuccess() {
+function drawAttackSuccess(player, cell, coordinates) {
+  player.board.receiveAttack(coordinates);
+  
+  cell.innerHTML += "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><title>sword-cross</title><path d='M6.2,2.44L18.1,14.34L20.22,12.22L21.63,13.63L19.16,16.1L22.34,19.28C22.73,19.67 22.73,20.3 22.34,20.69L21.63,21.4C21.24,21.79 20.61,21.79 20.22,21.4L17,18.23L14.56,20.7L13.15,19.29L15.27,17.17L3.37,5.27V2.44H6.2M15.89,10L20.63,5.26V2.44H17.8L13.06,7.18L15.89,10M10.94,15L8.11,12.13L5.9,14.34L3.78,12.22L2.37,13.63L4.84,16.1L1.66,19.29C1.27,19.68 1.27,20.31 1.66,20.7L2.37,21.41C2.76,21.8 3.39,21.8 3.78,21.41L7,18.23L9.44,20.7L10.85,19.29L8.73,17.17L10.94,15Z' /></svg>"
+  cell.classList = player.type == "Real" ? "cell sunk-self" : "cell sunk-enemy"
 
+  if (player.board.allShipSunk()) {
+    const gameOver = document.getElementById("game-over");
+
+    gameOver.innerText = player.type == "Real" ? "CPU Wins!" : "Player Wins!";
+  }
 }
 
-export default drawBoard;
+export { drawBoard, drawAttackFail, drawAttackSuccess };
