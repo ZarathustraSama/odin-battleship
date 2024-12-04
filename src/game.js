be1ae250bcdaaab9d236e0a89404ad1440de7b58
ZarathustraSama <mr.gabriel@live.it>
2024-12-04 19:45:46 +0100

    Properly initiates boards with event listeners, once player attacks, cpu immediately counters

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 import Player from "./player";
 import initializeBoard from "./initialize-board";
-import drawBoard from "./draw-board";
+import { drawBoard, drawAttackFail, drawAttackSuccess } from "./draw-board";
 
 function game() {
   const player = new Player("Real");
@@ -11,7 +11,21 @@ function game() {
   initializeBoard(player);
   initializeBoard(cpu);
 
-  drawBoard(player.board)
+  drawBoard(player);
+  drawBoard(cpu);
+
+  document.getElementById("randomizer").addEventListener("click", randomizePlayerBoard);
+
+  const cpuCells = document.querySelectorAll("#player-2 div .cell");
+  for (const [_, cell] of Object.entries(cpuCells)) {
+    cell.addEventListener("click", () => { cpu.attack() }, { once: true });
+  }
+}
+
+function randomizePlayerBoard() {
+  const player = new Player("Real");
+  initializeBoard(player);
+  drawBoard(player);
 }
 
 game();
